0b0b60b0d4540032ef554b52bef06fdabdc19782
JX <102360273+AddonVbs@users.noreply.github.com>
2025-05-14 22:32:00 +0700

    fix2

--- Back/main.go
+++ Back/main.go
@@ -12,11 +12,21 @@ type task struct {
 }
 
 var Task string = "Содить за молоком"
+var main_task = task{}
 
 // 1
 func PostHendler(h echo.Context) error {
+func GETHendler(h echo.Context) error {
 
 	return h.JSON(http.StatusOK, map[string]string{"task": Task})
+	main_task = task{Tasks: Task}
+
+	return h.JSON(http.StatusOK, "Все ок")
+}
+
+func POSTHendler(h echo.Context) error {
+
+	return h.JSON(http.StatusOK, main_task)
 }
 
 func main() {
@@ -27,6 +37,8 @@ func main() {
 	e.Use(middleware.Logger())
 
 	e.GET("/myTask", PostHendler)
+	e.GET("/myTask", GETHendler)
+	e.POST("/myTask", POSTHendler)
 
 	e.Start("localhost:8080")
 
