c0cc4b9ab38f441447a318452462ef7c5ac9962c
JX <102360273+AddonVbs@users.noreply.github.com>
2025-05-14 21:21:12 +0700

    frix

--- Back/main.go
+++ Back/main.go
@@ -1,79 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
-	"github.com/Knetic/govaluate"
-	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
-
-type Calculation struct {
-	ID         string `json:"id"`
-	Expression string `json:"expression"`
-	Result     string `json:"result"`
-}
-
-type CalculationRequest struct {
-	Expression string `json:"expression"`
-}
-
-var calculations = []Calculation{}
-
-var Task string = ""
 
 type task struct {
 	Tasks string `json:"task"`
 }
 
-// 4
-func PostHendler(t task) string {
-	Task = t.Tasks
-	return Task
+var Task string = "Содить за молоком"
 
-}
+// 1
+func PostHendler(h echo.Context) error {
 
-func calculationExpression(expression string) (string, error) {
-	expr, err := govaluate.NewEvaluableExpression(expression)
-	if err != nil {
-		return "", err
-	}
-	result, err := expr.Evaluate(nil)
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("%v", result), err
+	return h.JSON(http.StatusOK, map[string]string{"task": Task})
 
-}
-func postCalculation(c echo.Context) error {
-	var req CalculationRequest
-	err := c.Bind(&req)
-
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "invaild request"})
-	}
-
-	result, err := calculationExpression(req.Expression)
-
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "invalid Expression"})
-	}
-
-	calc := Calculation{
-		ID:         uuid.NewString(),
-		Expression: req.Expression,
-		Result:     result,
-	}
-	calculations = append(calculations, calc)
-
-	return c.JSON(http.StatusCreated, calc)
-
-}
-
-func getCalculations(c echo.Context) error {
-	return c.JSON(http.StatusOK, calculations)
 }
 
 func main() {
@@ -82,9 +26,8 @@ func main() {
 	e.Use(middleware.CORS())
 	e.Use(middleware.Logger())
 
-	e.GET("/calculations", getCalculations)
+	e.GET("/myTask", PostHendler)
 
-	e.POST("/calculations", postCalculation)
 	e.Start("localhost:8080")
 
 }
