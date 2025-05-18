46c80d0cbcc19f99ee1078a9715d305a810304b5
JX <make231107@gmail.com>
2025-05-18 20:06:45 +0700

    db

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
+func initDB() {
+	dsn := "host=localhost user=postgres password=yourpassword dbname=postgres port=5432 sslmode=disable"
+	var err error
+
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("Could not connect to DB: %v", err)
+	}
+
+	if err := db.AutoMigrate(&Task{}); err != nil {
+		log.Fatalf("Could not migrate: %v", err)
+
+	}
+
+}
+
 type Task struct {
-	ID    int    `json:"id"`
+	ID    int    `gorm:"primaryKey" json:"id"`
 	Task1 string `json:"task"`
 }
 
@@ -20,16 +42,15 @@ type Response struct {
 // all global var
 
 var IDtask = 1
-var my_task = make(map[int]Task)
 
 func GetHandler(c echo.Context) error {
-	var t []Task
+	var task []Task
 
-	for _, key := range my_task {
-		t = append(t, key)
-
+	if err := db.Find(&task).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, Response{Status: "error", Message: "Could not get task"})
 	}
-	return c.JSON(http.StatusOK, &t)
+
+	return c.JSON(http.StatusOK, &task)
 }
 
 func PostHandler(c echo.Context) error {
@@ -46,58 +67,62 @@ func PostHandler(c echo.Context) error {
 	t.ID = IDtask
 	IDtask++
 
-	my_task[t.ID] = t
+	if err := db.Create(&t).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, Response{Status: "Error", Message: "Could not add task"})
+	}
 
 	return c.JSON(http.StatusOK, t)
 }
 
 func PatchHandler(c echo.Context) error {
 	IDparam := c.Param("id")
-
 	id, err := strconv.Atoi(IDparam)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, Response{Status: "Bad", Message: "Invalid Patch !"})
 	}
-	var updataTask Task
 
-	if err := c.Bind(&updataTask); err != nil {
+	var updateTask Task
+	if err := c.Bind(&updateTask); err != nil {
 		return c.JSON(http.StatusBadRequest, Response{
 			Status:  "Error",
-			Message: "Could not updata Task",
+			Message: "Could not update Task",
 		})
 	}
 
-	if _, exist := my_task[id]; !exist {
-		return c.JSON(http.StatusBadRequest, Response{
+	var existingTask Task
+	if err := db.First(&existingTask, id).Error; err != nil {
+		return c.JSON(http.StatusNotFound, Response{
 			Status:  "Error",
-			Message: "Could not updata Task",
+			Message: "Task not found",
 		})
 	}
 
-	updataTask.ID = id
-	my_task[id] = updataTask
+	existingTask.Task1 = updateTask.Task1
+	if err := db.Save(&existingTask).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, Response{
+			Status:  "Error",
+			Message: "Could not update Task",
+		})
+	}
 
-	return c.JSON(http.StatusOK, updataTask)
-
+	return c.JSON(http.StatusOK, existingTask)
 }
 
 func Deletahendler(c echo.Context) error {
 	IDparam := c.Param("id")
-
 	id, err := strconv.Atoi(IDparam)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, Response{Status: "404"})
 	}
 
-	if _, exist := my_task[id]; !exist {
-		return c.JSON(http.StatusBadRequest, Response{
-			Status: "404",
+	if err := db.Delete(&Task{}, "id = ?", id).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, Response{
+			Status:  "Error",
+			Message: "Could not delete Task",
 		})
 	}
 
-	delete(my_task, id)
-	return c.JSON(http.StatusNoContent, Response{Status: "Success", Message: "Was delete "})
-
+	return c.JSON(http.StatusNoContent, Response{Status: "Success", Message: "Was deleted"})
 }
 
 func main() {
