1668a3e7da988220f9dc254ed0bd10aa096c11df
JX <make231107@gmail.com>
2025-05-23 20:54:33 +0700

    ь

--- internal/taskServers/repository.go
+++ internal/taskServers/repository.go
@@ -1,1 +1,47 @@
 package taskservers
+
+import (
+	"gorm.io/gorm"
+)
+
+type TaskRepository interface {
+	CreateTask(task Task) error
+	GetAllTask() ([]Task, error)
+	GetTaskByID(id string) (Task, error)
+	UpdateTask(task Task) error
+	DeleteTask(id string) error
+}
+
+type RepositorysTasks struct {
+	db *gorm.DB
+}
+
+func NewTaskRepository(db *gorm.DB) TaskRepository {
+	return &RepositorysTasks{db: db}
+}
+
+func (r *RepositorysTasks) CreateTask(task Task) error {
+	return r.db.Create(&task).Error
+}
+
+func (r *RepositorysTasks) GetAllTask() ([]Task, error) {
+	var tasks []Task
+	err := r.db.Find(&tasks).Error
+	return tasks, err
+
+}
+
+func (r *RepositorysTasks) GetTaskByID(id string) (Task, error) {
+	var t Task
+	err := r.db.First(&t, "id = ?", id).Error
+	return t, err
+}
+
+func (r *RepositorysTasks) UpdateTask(task Task) error {
+	return r.db.Save(&task).Error
+
+}
+
+func (r *RepositorysTasks) DeleteTask(id string) error {
+	return r.db.Delete(&Task{}, "id = ? ", id).Error
+}
--- internal/taskServers/service.go
+++ internal/taskServers/service.go
@@ -1,1 +1,37 @@
 package taskservers
+
+type TaskServers interface {
+	CreateTask(expression string) (Task, error)
+	GetAllTask() ([]Task, error)
+	GetTaskByID(id string) (Task, error)
+	UpdataTask(id, expression string) (Task, error)
+	DeleteTask(id string) error
+}
+
+type taskService struct {
+	repo TaskRepository
+}
+
+func NewTaskService(r TaskRepository) taskService {
+	return &taskService{repo: r}
+}
+
+func (s *taskService) CreateTask(expression string) (Task, error) {
+
+}
+
+func (s *taskService) GetAllTask() ([]Task, error) {
+
+}
+
+func (s *taskService) GetTaskByID(id string) (Task, error) {
+
+}
+
+func (s *taskService) UpdataTask(id, expression string) (Task, error) {
+
+}
+
+func (s *taskService) DeleteTask(id string) error {
+
+}
